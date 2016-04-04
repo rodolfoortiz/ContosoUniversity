ebcc9f9ff569e5d59677159383e93f633d6ac3b3
Rodolfo Ortiz <rodolfo.ortiz@headspring.com>
2016-04-04 14:35:25 -0500

    fixing version #

--- default.ps1
+++ default.ps1
@@ -116,8 +116,10 @@ task UpdateDatabase {
     deploy-database "Update" $devConnectionString $db_scripts_dir "DEV"
 }
 
-task OctoPack {
+task OctoPack -depends Clean {
+Write-Host "Init OctoPack"
     exec { msbuild.exe /t:build /v:q /p:Configuration=$project_config /nologo $source_dir\$project_name.sln /p:RunOctoPack=true /p:OctoPackPublishPackageToFileShare=..\\octopackages /p:OctoPackPackageVersion=$version}
+	Write-Host "End OctoPack"
 }
 
 task UpdateTestDatabase {
